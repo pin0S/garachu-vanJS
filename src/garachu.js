af46bcad266a74ebdcfa587fad15607b7aea9161
pin0S <pete.g.lynch@gmail.com>
2020-12-28 09:54:27 +1100

    basic html, grateful form submit event handled for state

---
+++ src/garachu.js
@@ -0,0 +1,27 @@
+const gratefulForm = document.querySelector('.grateful');
+const gratefulList = document.querySelector('.grateful-list');
+
+let gratefuls = []
+
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log('submitted!');
+    const grateful = e.currentTarget.grateful.value;
+
+    if (!grateful) return;
+
+    const item = {
+        grateful,
+        id: Date.now(),
+        complete: false,
+    };
+    // Push the items into our state
+    gratefuls.push(item);
+    console.log(`There are now ${gratefuls.length} in your state`);
+    // Clear the form
+    e.target.reset();
+    // fire off a custom event that will tell anyone else who cares that the items have been updated!
+    
+}
+
+gratefulForm.addEventListener('submit', handleSubmit);

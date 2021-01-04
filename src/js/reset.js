f438607d5dd71ef18dc3a5eef119e5b338cbbe90
pin0S <pete.g.lynch@gmail.com>
2021-01-04 17:02:48 +1100

    reset function improved to check on day rather than time

--- src/js/reset.js
+++ src/js/reset.js
@@ -1,20 +1,39 @@
-let nextMidnight = new Date();
-nextMidnight.setHours(24,0,0,0);
+function clearDailyData(tomorrow) {
 
-let now = new Date();
-let remainingTimeInSeconds = (nextMidnight.getTime() - now.getTime())
-console.log(`Midnight: ${nextMidnight}, Current: ${now}, Diff: ${remainingTimeInSeconds}`)
+    let now = new Date()
 
+    if (now.getDay == tomorrow.getDay()) {
+        window.localStorage.removeItem('currentGratefuls');
+        window.localStorage.removeItem('todaysQuote');
 
-function clearDailyData() {
-    
-    window.localStorage.removeItem('currentGratefuls');
-    window.localStorage.removeItem('todaysQuote');
-    
-    console.log('data is refreshed')
+        today = new Date();
+        let tomorrowLS = new Date();
+        tomorrowLS.setDate(today.getDate() + 1);
+
+        addTomorrowToLS(tomorrowLS)
+        console.log('data refreshed')
+    }
 }
 
-setInterval(clearDailyData, remainingTimeInSeconds);
+function addTomorrowToLS(tomorrow) {
+    localStorage.setItem("tomorrow", JSON.stringify(tomorrow));
+}
+
+try {
+    if (!JSON.parse(localStorage.getItem('tomorrow'))) {
+        let today = new Date();
+        let tomorrow = new Date();
+        tomorrow.setDate(today.getDate() + 1);
+        localStorage.setItem("tomorrow", JSON.stringify(tomorrow));
+        clearDailyData(tomorrow)
+    } else {
+        clearDailyData(JSON.parse(localStorage.getItem('tomorrow')))
+    } 
+} catch(e) {
+    console.log(e.message)
+}
+
+
 
 
 
